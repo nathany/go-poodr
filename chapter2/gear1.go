b3adb163efe3b7be5a30ebf80d44fc9665b6f892
Nathan Youngman <git@nathany.com>
2012-11-22 20:09:34 -0700

    the first Gear example

---
+++ chapter2/gear1.go
@@ -0,0 +1,22 @@
+// ############## Page 19 ##############
+package main
+
+import "fmt"
+
+type Gear struct {
+  Chainring float64
+  Cog       float64
+}
+
+func NewGear(chainring, cog float64) Gear {
+  return Gear{chainring, cog}
+}
+
+func (gear Gear) Ratio() float64 {
+  return gear.Chainring / gear.Cog
+}
+
+func main() {
+  fmt.Println(NewGear(52, 11).Ratio()) // => 4.7272727272727275
+  fmt.Println(NewGear(30, 27).Ratio()) // => 1.1111111111111112
+}
