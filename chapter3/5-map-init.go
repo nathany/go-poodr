3e4cabb164d907876caead2ebb3e943d7c0c02d3
Nathan Youngman <git@nathany.com>
2012-11-23 22:37:38 -0700

    isolate defaults()
    particularly if more than just simple literals

--- chapter3/5-map-init.go
+++ chapter3/5-map-init.go
@@ -4,6 +4,20 @@ package main
 import "fmt"
 
 /*
+  Options
+*/
+type Options map[string]interface{}
+
+// merge options, but don't overwrite what's there (unlike Ruby merge)
+func (options Options) merge(defaults Options) {
+  for k, v := range defaults {
+    if _, present := options[k]; !present {
+      options[k] = v
+    }
+  }
+}
+
+/*
   Gear
 */
 type Gear struct {
@@ -15,24 +29,15 @@ type Diameter interface {
   Diameter() float64
 }
 
-type Options map[string]interface{}
-
 /*
-
   This is how one could do hash-based initialization to remove argument order
   dependence like in the Ruby example.
 */
 func NewGear(options Options) *Gear {
-  chainring, ok := options["chainring"].(float64)
-  if !ok {
-    chainring = 40
-  }
+  options.merge(defaults())
 
-  cog, ok := options["cog"].(float64)
-  if !ok {
-    cog = 18
-  }
-
+  chainring, _ := options["chainring"].(float64)
+  cog, _ := options["cog"].(float64)
   wheel, _ := options["wheel"].(Diameter) // default nil
 
   /*
@@ -41,6 +46,13 @@ func NewGear(options Options) *Gear {
      though some setup would be necessary if 0-value defaults are not enough.
   */
   return &Gear{Chainring: chainring, Cog: cog, Wheel: wheel}
+}
+
+func defaults() Options {
+  return Options{
+    "chainring": 40.0, // these must be floats for the type assertion!
+    "cog":       18.0,
+  }
 }
 
 func (gear Gear) GearInches() float64 {
@@ -80,7 +92,8 @@ func main() {
   gear := NewGear(Options{
     "chainring": 52.0, // these must be floats for the type assertion!
     "cog":       11.0,
-    "wheel":     NewWheel(26, 1.5)})
+    "wheel":     NewWheel(26, 1.5),
+  })
   fmt.Println(gear.GearInches()) // => 137.0909090909091
   fmt.Println(gear.Ratio())      // => 4.7272727272727275
 }
